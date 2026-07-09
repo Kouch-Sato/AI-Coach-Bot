5f5f6d559522d8c028ddbfd68d0659cf716f44e6
Kouch-Sato <redkouchred@gmail.com>
2026-07-09 17:00:01 +0900

    mnist

--- 03-PyTorch-Practice/05.py
+++ 03-PyTorch-Practice/05.py
@@ -5,7 +5,6 @@
 
 
 import torch
-from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
@@ -15,4 +14,42 @@ test_datasets  = datasets.MNIST(root="data", train=False, download=True, transfo
 
 train_loader = DataLoader(train_datasets, batch_size=64, shuffle=True, num_workers=0)
 test_loader  = DataLoader(test_datasets, batch_size=256, shuffle=False, num_workers=0)
+
+model = torch.nn.Sequential(
+    torch.nn.Flatten(),
+    torch.nn.Linear(in_features = 28 * 28, out_features = 128),
+    torch.nn.ReLU(),
+    torch.nn.Linear(in_features = 128, out_features = 64),
+    torch.nn.ReLU(),
+    torch.nn.Linear(in_features = 64, out_features = 10),
+)
+
+# 回帰ではなく分類なので、交差エントロピーを使う
+loss_fn = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
+
+model.train()
+
+for epoch in range(10):
+    for x, y in train_loader:
+        pred_y = model(x)
+        loss = loss_fn(pred_y, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+model.eval()
+
+correct = 0
+total = 0
+
+for x, y in test_loader:
+    pred_y = model(x).argmax(dim = 1)
+    correct += (pred_y == y).sum().item()
+    total += len(y)
+
+accuracy = correct / total
+print (f"{accuracy * 100}%")
 
